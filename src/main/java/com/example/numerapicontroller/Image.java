46b10c2761c95aa8c8d17c92cfccbde2a304a6fe
Sanmandresy <hei.sanda.3@gmail.com>
2023-03-14 11:44:37 +0300

    chore: reformat code

--- src/main/java/com/example/numerapicontroller/Image.java
+++ src/main/java/com/example/numerapicontroller/Image.java
@@ -1,63 +1,51 @@
 package com.example.numerapicontroller;
 
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-import javax.imageio.ImageIO;
-import java.awt.*;
+import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import javax.imageio.ImageIO;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
 
 public class Image {
-    private String name;
-    private long size;
-    private byte[] bytes;
+  private String name;
+  private long size;
+  private byte[] bytes;
 
+  public static byte[] changeColorToGray(Image image) throws IOException {
+    InputStream input = new ByteArrayInputStream(image.getBytes());
+    BufferedImage saved = ImageIO.read(input);
+    int width = saved.getWidth(), height = saved.getHeight();
 
-    public void setAll(String name,long size,byte[] bytes){
-        this.name = name;
-        this.size = size;
-        this.bytes = bytes;
+    for (int y = 0; y < height; y++) {
+      for (int x = 0; x < width; x++) {
+        Color color = new Color(saved.getRGB(x, y));
+        int red = (int) (color.getRed() * 0.2126),
+            green = (int) (color.getGreen() * 0.7152),
+            blue = (int) (color.getBlue() * 0.0722),
+            sum = red + green + blue;
+        Color grayShade = new Color(sum, sum, sum);
+        saved.setRGB(x, y, grayShade.getRGB());
+      }
     }
+    input.close();
+
+    ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+    ImageIO.write(saved, "jpg", outputStream);
 
 
-    public byte[] changeColorToGray(Image image) throws IOException {
-        InputStream input = new ByteArrayInputStream(image.getBytes());
-        BufferedImage saved = ImageIO.read(input);
-        int width = saved.getWidth(), height = saved.getHeight();
+    outputStream.flush();
+    byte[] imageAsGray = outputStream.toByteArray();
+    outputStream.close();
 
-        for(int y = 0 ; y < height; y++){
-            for(int x = 0; x < width; x++){
-                Color color = new Color(saved.getRGB(x,y));
-                int red = (int)(color.getRed() * 0.2126),
-                        green = (int)(color.getGreen() * 0.7152),
-                        blue = (int)(color.getBlue() * 0.0722),
-                        sum = red + green + blue;
-                Color grayShade= new Color(sum,sum,sum);
-                saved.setRGB(x,y,grayShade.getRGB());
-            }
-        }
-        input.close();
-
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        ImageIO.write(saved,"jpg",outputStream);
-
-
-        outputStream.flush();
-        byte[] imageAsGray = outputStream.toByteArray();
-        outputStream.close();
-
-        return imageAsGray;
-    }
-
-
+    return imageAsGray;
+  }
 
 }
--- src/main/java/com/example/numerapicontroller/PostController.java
+++ src/main/java/com/example/numerapicontroller/PostController.java
@@ -1,19 +1,23 @@
 package com.example.numerapicontroller;
 
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import static com.example.numerapicontroller.Image.changeColorToGray;
 
 
 @RestController
 public class PostController {
-    @PostMapping(path = "/image",
-            consumes = {MediaType.MULTIPART_FORM_DATA_VALUE},
-            produces = {MediaType.IMAGE_PNG_VALUE,MediaType.IMAGE_JPEG_VALUE})
-    public byte[] getClientImage(@RequestParam("file")MultipartFile file) throws IOException {
-            Image image = new Image(file.getOriginalFilename(),file.getSize(), file.getBytes());
-            return image.changeColorToGray(image);
-    }
+  @PostMapping(path = "/image",
+      consumes = {MediaType.MULTIPART_FORM_DATA_VALUE},
+      produces = {MediaType.IMAGE_PNG_VALUE, MediaType.IMAGE_JPEG_VALUE})
+  public byte[] getClientImage(@RequestParam("file") MultipartFile file) throws IOException {
+    Image image = new Image(file.getOriginalFilename(), file.getSize(), file.getBytes());
+    return changeColorToGray(image);
+  }
 
 }
